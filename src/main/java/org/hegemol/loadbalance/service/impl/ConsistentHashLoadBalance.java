2d8ed5e01bfc21412c2909cf87734b45d0e5fd1c
KevinClair <kevinclair@apache.org>
2022-09-25 15:24:16 +0800

    一致性hash算法实现

---
+++ src/main/java/org/hegemol/loadbalance/service/impl/ConsistentHashLoadBalance.java
@@ -0,0 +1,59 @@
+package org.hegemol.loadbalance.service.impl;
+
+import org.hegemol.loadbalance.model.ConsistentHashInstance;
+import org.hegemol.loadbalance.model.Instance;
+import org.hegemol.loadbalance.service.AbstractLoadBalance;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ * Hash负载均衡
+ *
+ * @author KevinClair
+ **/
+public class ConsistentHashLoadBalance extends AbstractLoadBalance<ConsistentHashInstance> {
+
+    private static final String INSTANCE = "-INSTANCE-";
+
+    @Override
+    protected Instance doLoad(final List<ConsistentHashInstance> instances, final String ip) {
+        TreeMap<Long, Instance> treeMap = new TreeMap<>();
+        instances.forEach(each -> {
+            for (int i = 0; i < each.getFictitiousInstance()-1; i++) {
+                Long hashKey = hash(each.getAddress() + INSTANCE + i);
+                treeMap.put(hashKey, each);
+            }
+        });
+        long clientHash = hash(ip);
+        SortedMap<Long, Instance> sortedMap = treeMap.tailMap(clientHash);
+        if (!sortedMap.isEmpty()) {
+            return sortedMap.get(sortedMap.firstKey());
+        }
+        return treeMap.firstEntry().getValue();
+    }
+
+    private static long hash(final String key) {
+        // md5 byte
+        MessageDigest md5;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 not supported", e);
+        }
+        md5.reset();
+        md5.update(key.getBytes(StandardCharsets.UTF_8));
+        byte[] digest = md5.digest();
+        // hash code, Truncate to 32-bits
+        long h = 0;
+        for (int i = 0; i < 4; i++) {
+            h <<= 8;
+            h |= ((int) digest[i]) & 0xFF;
+        }
+        return h;
+    }
+}
